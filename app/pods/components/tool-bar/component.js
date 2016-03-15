8efb28c5f03ecda5f89a3274c514f8d955ac6aba
byelipk <byelipk@gmail.com>
2016-03-15 18:41:05 -0400

    add invisible input elements

---
+++ app/pods/components/tool-bar/component.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['btn-toolbar', 'm-b-1'],
+  attributeBindings: ['label:aria-label'],
+  ariaRole: 'toolbar',
+  label: 'Toolbar with button groups',
+
+  actions: {
+    openFilePicker() {
+      // Simulate click on file input field
+      document.querySelector("#file").click();
+    },
+
+    openDirectoryPicker() {
+      // Simulate click on file input field
+      document.querySelector("#directory").click();
+    },
+
+    handleFiles(evt) {
+      let files = evt.target.files;
+      if (files.length > 0) {
+        // this.send('onFileSelect', evt.target.files;)
+        console.log("What do we do now?");
+      }
+    }
+  }
+});
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -39,5 +39,12 @@ module.exports = function(defaults) {
     destDir: 'assets'
   });
 
+  app.import('bower_components/bootstrap/js/dist/util.js');
+  app.import('bower_components/bootstrap/js/dist/util.js.map');
+  // app.import('bower_components/bootstrap/js/dist/button.js');
+  // app.import('bower_components/bootstrap/js/dist/button.js.map');
+  app.import('bower_components/bootstrap/js/dist/dropdown.js');
+  app.import('bower_components/bootstrap/js/dist/dropdown.js.map');
+
   return app.toTree();
 };
