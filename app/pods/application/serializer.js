a2329ca6b797d0a98bed16870dec1f44822a951a
byelipk <byelipk@gmail.com>
2016-04-11 12:10:17 -0400

    no need to override serializer

--- app/pods/application/serializer.js
+++ app/pods/application/serializer.js
@@ -1,14 +1,21 @@
 import Ember from 'ember';
 import DS from 'ember-data';
 
-var underscore = Ember.String.underscore;
+
+// NOTE
+// If we're ever having problems serializing
+// the payload from the server into a proper
+// JSONAPI document, we can fix it with these
+// two hooks:
+
+let underscore = Ember.String.underscore;
 
 export default DS.JSONAPISerializer.extend({
-  keyForAttribute: function(attr) {
-    return underscore(attr);
-  },
+  // keyForAttribute: function(attr) {
+  //   return underscore(attr);
+  // },
 
-  keyForRelationship: function(rawKey) {
-    return underscore(rawKey);
-  }
+  // keyForRelationship: function(rawKey) {
+  //   return underscore(rawKey);
+  // }
 });
