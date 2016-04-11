eb587031d0483cc0965e0881a7ec8f8ac8a099d6
byelipk <byelipk@gmail.com>
2016-04-11 17:31:38 -0400

    ember-cable works

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -1,12 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  notify:       Ember.inject.service('notify'),
   cableService: Ember.inject.service('cable'),
 
   initCableService: Ember.on('init', function () {
+    // Create a consumer
+    let notify   = this.get('notify');
     let consumer = this.get('cableService').createConsumer("ws://localhost:4200/cable");
 
-    console.log(consumer);
+    // Create any number of subscriptions
+    consumer.subscriptions.create({
+      channel: "AppearancesChannel",
+      room: "Welcome"
+    }, {
+      connected() {
+        notify.info("You're connected via WebSockets!");
+      },
+
+      received(data) {
+        notify.success(data['body'], {
+          closeAfter: 10000
+        });
+      },
+
+      disconnected() {
+        Ember.debug("AppearancesChannel#disconnected");
+      }
+    });
   })
 
 });
